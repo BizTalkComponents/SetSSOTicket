11550800cf6334efc703db4031b6004a0748414a
robinhultman <robin.hultman@gmail.com>
2014-10-04 16:43:54 +0200

    Implemented SSOTicket component

---
+++ SetSSOTicket/SetSSOTicket.Component.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace BizTalkComponents.PipelineComponents.SetSSOTicket
+{
+    public partial class SetSSOTicket
+    {
+        public string Name { get { return "SetSSOTicket"; } }
+        public string Version { get { return "1.0"; } }
+        public string Description { get { return "Issues and promotes a new SSO Ticket"; } }
+
+        public IEnumerator Validate(object projectSystem)
+        {
+            return null;
+        }
+
+        public IntPtr Icon { get { return IntPtr.Zero; } }
+    }
+}
--- SetSSOTicket/SetSSOTicket.cs
+++ SetSSOTicket/SetSSOTicket.cs
@@ -1,6 +1,22 @@
-﻿namespace BizTalkComponents.PipelineComponents.SetSSOTicket
+﻿using Microsoft.BizTalk.Component.Interop;
+using Microsoft.BizTalk.Message.Interop;
+using Microsoft.EnterpriseSingleSignOn.Interop;
+using BizTalkComponents.Utils.ContextPropertyHelpers;
+
+namespace BizTalkComponents.PipelineComponents.SetSSOTicket
 {
-    public class SetSSOTicket
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [System.Runtime.InteropServices.Guid("84CA57A3-74AC-4262-B558-0B75B6D898F7")]
+    [ComponentCategory(CategoryTypes.CATID_Encoder)]
+    public partial class SetSSOTicket : IComponent, IBaseComponent, IComponentUI
     {
+        public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
+        {
+            var ticket = new ISSOTicket();
+
+            pInMsg.Context.Write(new ContextProperty(SSOTicketProperties.SSOTicket), ticket.IssueTicket(0));
+
+            return pInMsg;
+        }
     }
 }
